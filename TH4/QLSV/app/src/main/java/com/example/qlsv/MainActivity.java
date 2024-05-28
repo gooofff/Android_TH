ca74a2d8248096f4ec69799cea295be24610c063
CuongTran <cuongtraan212@gmail.com>
2024-05-28 11:09:25 +0700

    Giao dien

--- TH4/QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
+++ TH4/QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.qlsv;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtName, edtEmail;
     private Button btnAdd, btnUpdate, btnDel;
+    private ListView listView;
+    private List<Student> studentList;
+    private FirebaseDatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +38,77 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        edtName = (EditText) findViewById(R.id.edtName);
+        edtEmail = (EditText) findViewById(R.id.edtEmail);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        btnDel = (Button) findViewById(R.id.btnDel);
+        listView = (ListView) findViewById(R.id.listView);
+        studentList = new ArrayList<>();
+        databaseHelper = new FirebaseDatabaseHelper();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addStudent();
+            }
+        });
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateStudent();
+            }
+        });
+
+        btnDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteStudent();
+            }
+        });
+
+        loadStudents();
     }
+
+    private void addStudent() {
+        String name = edtName.getText().toString();
+        String email = edtEmail.getText().toString();
+        String id = databaseHelper.getReference().push().getKey();
+        Student student = new Student(id, name, email);
+        databaseHelper.addStudent(student);
+    }
+    private void updateStudent() {
+        String id = "";
+        String name = edtName.getText().toString();
+        String email = edtEmail.getText().toString();
+        Student student = new Student(id, name, email);
+        databaseHelper.updateStudent(id, student);
+    }
+    private void deleteStudent() {
+        String id = "";
+        databaseHelper.deleteStudent(id);
+    }
+    private void loadStudents() {
+        databaseHelper.getReference().addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                studentList.clear();
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Student student = postSnapshot.getValue(Student.class);
+                    studentList.add(student);
+                }
+
+                // Update ListView adapter here
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
 
 }
