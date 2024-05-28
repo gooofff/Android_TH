145235ffa4310e4eccb9bb9dba0d79cb6b382482
CuongTran <cuongtraan212@gmail.com>
2024-05-28 12:12:30 +0700

    Hoan thanh tinh nang

--- TH4/QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
+++ TH4/QLSV/app/src/main/java/com/example/qlsv/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.qlsv;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -23,10 +26,14 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText edtName, edtEmail;
-    private Button btnAdd, btnUpdate, btnDel;
+    private Button btnAdd, btnUpdate, btnDel, btnClear;
     private ListView listView;
     private List<Student> studentList;
+    private List<String> list;
     private FirebaseDatabaseHelper databaseHelper;
+    ArrayAdapter<String> adapter;
+    private String StudentID = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +51,9 @@ public class MainActivity extends AppCompatActivity {
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnUpdate = (Button) findViewById(R.id.btnUpdate);
         btnDel = (Button) findViewById(R.id.btnDel);
+        btnClear = (Button) findViewById(R.id.btnClear);
         listView = (ListView) findViewById(R.id.listView);
+        list = new ArrayList<>();
         studentList = new ArrayList<>();
         databaseHelper = new FirebaseDatabaseHelper();
 
@@ -69,38 +78,85 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                edtName.setText("");
+                edtEmail.setText("");
+                loadStudents();
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String) parent.getItemAtPosition(position);
+                String[] parts = selectedItem.split(" - ");
+                if (parts.length == 3) {
+                    StudentID = parts[0];
+                    String name = parts[1];
+                    String email = parts[2];
+                    edtName.setText(name);
+                    edtEmail.setText((email));
+                    adapter.clear();
+                }
+                loadStudents();
+            }
+        });
+
         loadStudents();
     }
 
     private void addStudent() {
         String name = edtName.getText().toString();
         String email = edtEmail.getText().toString();
-        String id = databaseHelper.getReference().push().getKey();
-        Student student = new Student(id, name, email);
-        databaseHelper.addStudent(student);
+        if (name.equals("") || email.equals("")) {
+            Toast.makeText(this, "Please enter a name and email", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            String id = databaseHelper.getReference().push().getKey();
+            Student student = new Student(id, name, email);
+            StudentID = "";
+            databaseHelper.addStudent(student);
+        }
     }
     private void updateStudent() {
-        String id = "";
         String name = edtName.getText().toString();
         String email = edtEmail.getText().toString();
-        Student student = new Student(id, name, email);
-        databaseHelper.updateStudent(id, student);
+        Student student = new Student(StudentID, name, email);
+        databaseHelper.updateStudent(StudentID, student);
+        loadStudents();
     }
     private void deleteStudent() {
-        String id = "";
-        databaseHelper.deleteStudent(id);
+        if (StudentID.isEmpty()) {
+            Toast.makeText(this, "Please select a student to delete", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            databaseHelper.deleteStudent(StudentID);
+            StudentID = "";
+            edtName.setText("");
+            edtEmail.setText("");
+            loadStudents();
+        }
     }
     private void loadStudents() {
         databaseHelper.getReference().addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 studentList.clear();
+                list.clear();
                 for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
                     Student student = postSnapshot.getValue(Student.class);
                     studentList.add(student);
                 }
 
-                // Update ListView adapter here
+                for (Student student : studentList) {
+                    String temp = student.getId() + " - " + student.getName() + " - " + student.getEmail();
+                    list.add(temp);
+                }
+
+                adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, list);
+                listView.setAdapter(adapter);
             }
 
             @Override
@@ -109,6 +165,4 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-
 }
